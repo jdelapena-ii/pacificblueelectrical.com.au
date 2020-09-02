8a35b4f9e3c23c65aac450dd576ea97fb239c2e7
Luke Bennett <hello@lukebennett.com.au>
2020-09-02 15:05:38 +1000

    Add icons to footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,31 +1,24 @@
 import React from 'react';
-import { Link } from 'gatsby';
 
-import { useGraphQL } from '../hooks';
-import { socialLinks } from '../data/site-navigation';
+import { socialLinks } from '../data';
 
-export function Footer() {
-  const {
-    site: {
-      siteMetadata: { title },
-    },
-  } = useGraphQL();
+function Footer() {
   return (
     <footer className="bg-brand-black">
       <div className="max-w-screen-xl px-4 mx-auto overflow-hidden sm:px-6 lg:px-8">
         <div className="grid grid-cols-4">
-          {/* <div className="flex justify-center mt-8 space-x-6">
+          <div className="flex justify-center mt-8 space-x-6">
             {socialLinks.map((node) => (
               <a
                 key={node.id}
                 href={node.url}
-                className="text-gray-400 transition duration-150 ease-in-out hover:text-gray-500"
+                className="text-white transition duration-150 ease-in-out hover:text-gray-300"
               >
                 <span className="sr-only">{node.label}</span>
                 <node.icon className="w-6 h-6" />
               </a>
             ))}
-          </div> */}
+          </div>
         </div>
         <div className="py-2 mt-8 border-t border-white border-solid">
           <a
@@ -34,10 +27,12 @@ export function Footer() {
             rel="noreferrer"
             className="block text-sm leading-6 text-center text-white"
           >
-            Wesbite by Phiranno Designs
+            Website by Phiranno Designs
           </a>
         </div>
       </div>
     </footer>
   );
 }
+
+export { Footer };
