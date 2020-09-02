31b0c8eee67303dbf45f453d46825618b22eb802
Jd <manabat.johnder@gmail.com>
2020-09-02 13:55:52 +0800

    changing branch

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,12 +4,10 @@ import { Layout, SEO, Contact, Hero } from '../components';
 import { useGraphQL } from '../hooks';
 
 export default function IndexPage() {
-  const { omnicare } = useGraphQL();
   return (
     <Layout>
       <SEO title="Home" />
-      <Hero image={omnicare.childImageSharp.fluid} />
-      <h1 className="font-serif">Hello world!</h1>
+      <Contact />
     </Layout>
   );
 }
