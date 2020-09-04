93e8f6976fa31028287ce5416e067d67a03d8462
Jedo <jtdelapenaii@gmail.com>
2020-09-04 14:20:56 +0800

    map image with text overlay

--- src/components/map-image.js
+++ src/components/map-image.js
@@ -1,26 +1,31 @@
 import React from 'react';
-import { GatsbyImage } from 'gatsby-image';
+import GatsbyImage from 'gatsby-image';
 import PropTypes from 'prop-types';
 import { useGraphQL } from '../hooks';
 
 function MapImage() {
   const { pmqMap } = useGraphQL();
+
   return (
-    <div className="relative px-4 pt-12">
-      <PmqMap image={pmqMap.childImageSharp.fluid} />
+    <div className="grid gap-4 pt-20 bg-black md:grid-cols-2 lg:grid-cols-4">
+      <Image image={pmqMap.childImageSharp.fluid} />
     </div>
   );
 }
 
-function PmqMap({ image }) {
+function Image({ image }) {
   return (
-    <div className="absolute inset-0 flex">
-      <GatsbyImage fluid={image} className="flex-1" />
+    <div className="flex">
+      <GatsbyImage
+        fluid={image}
+        className="object-cover w-full h-full"
+        style={{ position: 'absolute' }}
+      />
     </div>
   );
 }
 
-MapImage.propTypes = {
+Image.propTypes = {
   image: PropTypes.object.isRequired,
 };
 
