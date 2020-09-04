a50cbf609dba2c8559579c86f099f64d0931f92f
Luke Bennett <hello@lukebennett.com.au>
2020-09-04 21:22:01 +1000

    Increase font-weight

--- src/components/form-elements/error.js
+++ src/components/form-elements/error.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 export function Error({ message }) {
   return (
-    <p role="alert" className="text-xs text-red-700 uppercase">
+    <p role="alert" className="text-xs font-semibold text-red-700 uppercase">
       {message}
     </p>
   );
