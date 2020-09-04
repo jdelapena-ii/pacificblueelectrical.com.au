d1541ea0656430b3aac7ed13767965c607d20fbe
Luke Bennett <hello@lukebennett.com.au>
2020-09-04 21:54:28 +1000

    Clean up spacing

--- src/components/contact.js
+++ src/components/contact.js
@@ -19,9 +19,9 @@ function Contact({
 
   return (
     <article
-      className={`grid gap-4 py-6 md:py-0 px-4 mb-12 sm:px-8 lg:px-20 md:grid-cols-2 ${bgColorClass}`}
+      className={`grid items-center py-6 md:py-0 lg:px-20 md:grid-cols-2 ${bgColorClass}`}
     >
-      <div className={`md:pt-12 md:pr-8 md:mb-0 mb-8 ${textColorClass}`}>
+      <div className={`px-4 pb-12 sm:px-8 ${textColorClass}`}>
         <h1 className="mb-2 font-serif text-5xl text-center md:text-6xl md:text-left">
           Contact us
         </h1>
@@ -57,57 +57,58 @@ function Contact({
           </div>
         </dl>
       </div>
-
-      <ContactForm
-        handleSubmit={handleSubmit}
-        register={register}
-        setIsSubmitting={setIsSubmitting}
-        action="/success/"
-        name="contact_form"
-        className="w-full max-w-lg p-6 mx-auto space-y-6 transform bg-white shadow-sm md:-translate-y-14"
-      >
-        <Input
-          label="Full Name"
-          name="full_name"
+      <div className="px-4 sm:px-0">
+        <ContactForm
+          handleSubmit={handleSubmit}
           register={register}
-          errors={errors}
-        />
-        <Input
-          label="Contact Number"
-          name="contact_number"
-          type="tel"
-          register={register}
-          errors={errors}
-        />
-        <Input
-          label="Email"
-          name="email_address"
-          register={register}
-          errors={errors}
-        />
-        <Select
-          name="inquiries"
-          label="What are you inquiring about?"
-          options={['Done deal', 'No deal', 'Other']}
-          register={register}
-          errors={errors}
-        />
-        <TextArea
-          label="Message"
-          name="message"
-          register={register}
-          errors={errors}
-        />
-        <div className="flex justify-end mt-4">
-          <button
-            type="submit"
-            disabled={isSubmitting}
-            className="px-12 py-2 text-sm font-bold leading-none transition duration-300 ease-in-out border border-black rounded shadow-sm justify-cente hover:bg-black hover:text-white focus:bg-white focus:text-black"
-          >
-            Submit
-          </button>
-        </div>
-      </ContactForm>
+          setIsSubmitting={setIsSubmitting}
+          action="/success/"
+          name="contact_form"
+          className="w-full max-w-lg p-6 mx-auto space-y-6 transform bg-white shadow-sm lg:max-w-none md:-translate-y-14"
+        >
+          <Input
+            label="Full Name"
+            name="full_name"
+            register={register}
+            errors={errors}
+          />
+          <Input
+            label="Contact Number"
+            name="contact_number"
+            type="tel"
+            register={register}
+            errors={errors}
+          />
+          <Input
+            label="Email"
+            name="email_address"
+            register={register}
+            errors={errors}
+          />
+          <Select
+            name="inquiries"
+            label="What are you inquiring about?"
+            options={['Done deal', 'No deal', 'Other']}
+            register={register}
+            errors={errors}
+          />
+          <TextArea
+            label="Message"
+            name="message"
+            register={register}
+            errors={errors}
+          />
+          <div className="flex justify-end mt-4">
+            <button
+              type="submit"
+              disabled={isSubmitting}
+              className="justify-center px-12 py-2 text-sm font-bold leading-none transition duration-300 ease-in-out border border-black rounded shadow-sm hover:bg-black hover:text-white focus:bg-white focus:text-black"
+            >
+              Submit
+            </button>
+          </div>
+        </ContactForm>
+      </div>
     </article>
   );
 }
