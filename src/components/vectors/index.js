c2dfd22570048445d6c9c9604a1ef1b3c73b85f8
Luke Bennett <hello@lukebennett.com.au>
2020-09-04 16:57:11 +1000

    Add optimised vectors

--- src/components/vectors/index.js
+++ src/components/vectors/index.js
@@ -1,4 +1,7 @@
+export * from './commercial-icon';
+export * from './domestic-icon';
 export * from './facebook-icon';
 export * from './instagram-icon';
+export * from './level-2-icon';
 export * from './logo-mobile';
 export * from './logo';
