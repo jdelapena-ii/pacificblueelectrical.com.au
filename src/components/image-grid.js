a668c0fb2854194bc415403b5ece228cbccabbb3
Jedo <jtdelapenaii@gmail.com>
2020-09-04 08:58:39 +0800

    adjusted grid arrangement

--- src/components/image-grid.js
+++ src/components/image-grid.js
@@ -6,7 +6,9 @@ import { useGraphQL } from '../hooks';
 function ImageGrid() {
   const { homeTwoImage } = useGraphQL();
   return (
-    <div className="grid bg-black md:grid-cols-2 lg:grid-cols-3">
+    <div className="grid bg-black md:grid-cols-2 lg:grid-cols-4">
+      <ServiceImage image={homeTwoImage.childImageSharp.fluid} />
+      <ServiceImage image={homeTwoImage.childImageSharp.fluid} />
       <ServiceImage image={homeTwoImage.childImageSharp.fluid} />
       <ServiceImage image={homeTwoImage.childImageSharp.fluid} />
       <ServiceImage image={homeTwoImage.childImageSharp.fluid} />
@@ -20,7 +22,9 @@ function ImageGrid() {
 function ServiceImage({ image }) {
   return (
     <div className="relative h-0 aspect-ratio-square">
-      <GatsbyImage fluid={image} className="flex-1" />
+      <div className="py-0 m-2 aspect-ratio-square">
+        <GatsbyImage fluid={image} className="flex-1" />
+      </div>
     </div>
   );
 }
